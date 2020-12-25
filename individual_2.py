06e0e1f2969c5d063f197ece82c4307231d54948
Alexander-fix <70954548+Alexander-fix@users.noreply.github.com>
2020-12-25 20:15:59 +0300

    Update individual_2.py

--- individual_2.py
+++ individual_2.py
@@ -1,39 +1,37 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import math
 import sys
 
-print("Введите коэффициенты для уравнения")
-a = float(input("a = "))
-if a == 0:
-    print("В данном уравнении a не может быть равно нулю", file=sys.stderr)
-    exit(1)
-b = float(input("b = "))
-c = float(input("c = "))
+if __name__ == '__main__':
+    print("Введите коэффициенты для уравнения")
+    a = float(input("a = "))
+    if a == 0:
+        print("В данном уравнении a не может быть равно нулю", file=sys.stderr)
+        exit(1)
+    b = float(input("b = "))
+    c = float(input("c = "))
 
-D = b ** 2 - 4 * a * c
+    D = b ** 2 - 4 * a * c
 
-if D > 0:
-    t1 = (-b + math.sqrt(D)) / (2 * a)
-    t2 = (-b - math.sqrt(D)) / (2 * a)
-    if t1 < 0 and t2 < 0:
-        print("Корней нет")
-    elif t1 < 0:
-        print("Найденные корни уравнения:", math.sqrt(t2), -math.sqrt(t2))
-    elif t2 < 0:
-        print("Найденные корни уравнения:", math.sqrt(t1), -math.sqrt(t1))
+    if D > 0:
+        t1 = (-b + math.sqrt(D)) / (2 * a)
+        t2 = (-b - math.sqrt(D)) / (2 * a)
+        if t1 < 0 and t2 < 0:
+            print("Корней нет")
+        elif t1 < 0:
+            print("Найденные корни уравнения:", math.sqrt(t2), -math.sqrt(t2))
+        elif t2 < 0:
+            print("Найденные корни уравнения:", math.sqrt(t1), -math.sqrt(t1))
+        else:
+            print("Найденные корни уравнения:", math.sqrt(t1), -math.sqrt(t1),
+                  math.sqrt(t2), -math.sqrt(t2))
+    elif D == 0:
+        t = -b / (2 * a)
+        if t < 0:
+            print("Корней нет")
+        elif t == 0:
+            print("Найденные корни уравнения:", -math.sqrt(t))
+        else:
+            print("Найденные корни уравнения:", math.sqrt(t), -math.sqrt(t))
+
     else:
-        print("Найденные корни уравнения:", math.sqrt(t1), -math.sqrt(t1),
-              math.sqrt(t2), -math.sqrt(t2))
-elif D == 0:
-    t = -b / (2 * a)
-    if t < 0:
         print("Корней нет")
-    elif t == 0:
-        print("Найденные корни уравнения:", -math.sqrt(t))
-    else:
-        print("Найденные корни уравнения:", math.sqrt(t), -math.sqrt(t))
-
-else:
-    print("Корней нет")
