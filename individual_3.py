68b9da1fd9650d5e2406ba6b2d39ccbbb7adabcb
Alexander-fix <70954548+Alexander-fix@users.noreply.github.com>
2020-12-25 20:18:32 +0300

    Update individual_3.py

--- individual_3.py
+++ individual_3.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-for a in range(10, 99):
-    b = a // 10
-    c = a % 10
+if __name__ == '__main__':
+    for a in range(10, 99):
+        b = a // 10
+        c = a % 10
 
-    if a % (b + c) == 0:
-        print(a, end=" ")
+        if a % (b + c) == 0:
+            print(a, end=" ")
